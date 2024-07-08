c92ca69a55cc37f5aa9b0a9f43cb3971c2766c70
Lucas Fernandes <lucasuzumaki69@hotmail.com>
2024-07-08 15:04:58 -0300

    Configurando o ambiente de desenvolvimento

---
+++ src/styles.ts
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components'
+
+const EstiloGobal = createGlobalStyle`
+*{
+padding: 0;
+Margin: 0;
+box-sizing: border-box;
+
+}
+`
+export default EstiloGobal
